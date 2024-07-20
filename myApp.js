00385ba99162ebab12825c157ef79c341ac0814e
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-07-20 10:11:58 -0500

    first

--- myApp.js
+++ myApp.js
@@ -1,39 +1,12 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+    res.sendFile(absolutePath = __dirname + '/views/index.html');
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/public", express.static(__dirname + "/public"));
+//css
  module.exports = app;
