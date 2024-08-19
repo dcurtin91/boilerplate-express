68b2246f1a2f6d9d977c5d0a9226569ed6a65de7
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-19 13:03:42 -0500

    restarting

--- myApp.js
+++ myApp.js
@@ -1,39 +1,19 @@
 let express = require('express');
 let app = express();
-require('dotenv').config()
 
-app.use(function middleware(req, res, next) {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-});
 
-app.get(
-  "/now",
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.send({
-      time: req.time
-    });
-  }
-);
-
-app.get("/", (req, res) => {
-    res.sendFile(absolutePath = __dirname + '/views/index.html');
+app.get("/", function(req, res) {
+    res.sendFile( __dirname + "/views/index.html");
   });
 
-app.get("/json",(req, res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase"){
-    res.json({"message": "HELLO JSON"})
-  }  else {
-  res.json({"message": "Hello json"})}
-});
+  app.use("/public", express.static(__dirname + "/public"));
 
 
-app.use("/public", express.static(__dirname + "/public"));
-//css
+  app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+
+
  module.exports = app;
-
- 
