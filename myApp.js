09714b8f313b773f3ee6deee407480b73e718d84
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-19 17:24:43 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,46 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/name', function(req, res) {
+  var firstName = req.body.first;
+  var lastName = req.body.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  })
+})
+
+app.use(function logger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next();
+});
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({
+    time: req.time});
+});
+
+app.get('/name', function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  })
+})
+
+app.get("/:word/echo", function(req, res) {
+
+  res.json({
+    echo: req.params.word
+  })
+})
 
 app.get("/", function(req, res) {
     res.sendFile( __dirname + "/views/index.html");
@@ -10,9 +50,15 @@ app.get("/", function(req, res) {
 
 
   app.get("/json", (req, res) => {
-    res.json({
-      message: "Hello json"
-    });
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({
+         message: "Hello json".toUpperCase()})
+    } else {
+      res.json({
+        message: "Hello json"
+      })
+    }
+    
   });
 
 
